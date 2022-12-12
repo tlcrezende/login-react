c389c28b6739ab0cc9255a9a90e8ea244a9ec901
Thiago <tlcrezende@gmail.com>
2022-12-12 18:32:26 -0300

    Css parcialmente completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-
+import jpIMG from './assets/jp.svg'
+import './styles.css'
 
 function App() {
   return (
-    <h1>Hello World teste</h1>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo!</span>
+
+            <span className="login-form-title">
+              <img src={jpIMG} alt="Programdor Thiago"></img>
+            </span>
+
+            <div className="wrap-input">
+              <input type="email" />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+            <div className="wrap-input">
+              <input type="password" />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div classNome="Container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
